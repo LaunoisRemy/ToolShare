5fc5d411cdeb5f2afea56c6c7eb262bbdd73baba
LaunoisRemy <remy.launois@gmail.com>
2020-11-29 16:07:47 +0100

    ADD : Session class
    Session class represent the session connect in the system
    Singelton beacause user must be unique

---
+++ src/main/java/business/system/Session.java
@@ -0,0 +1,36 @@
+package business.system;
+
+import db.ConnectionDBMySQL;
+
+import java.sql.Connection;
+
+public class Session {
+    private User user;
+
+    /**
+     * The static class definition LazyHolder within it is not initialized until the JVM determines that LazyHolder must be executed
+     */
+    private static class LazyHolder {
+        public static final Session INSTANCE= new Session();
+    }
+
+    /**
+     * getInstance will return the same correctly initialized INSTANCE
+     * @return instance of the class
+     */
+    public static Session getInstance(){
+        return Session.LazyHolder.INSTANCE;
+    }
+
+    /**
+     * Method return user connected
+     * @return user
+     */
+    public User getUser(){
+        return user;
+    };
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
