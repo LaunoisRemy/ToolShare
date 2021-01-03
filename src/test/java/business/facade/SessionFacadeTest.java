37699e5de73e6c40e97a33b475e89eff90bd9e92
LaunoisRemy <remy.launois@gmail.com>
2021-01-03 10:00:25 +0100

    TEST : all test login SessionFacade

--- src/test/java/business/facade/SessionFacadeTest.java
+++ src/test/java/business/facade/SessionFacadeTest.java
@@ -39,6 +39,36 @@ class SessionFacadeTest {
 
 
     @Test
+    public void login_userDoesntExist_throwObjectNotFoundException() {
+        try(MockedStatic<UserDAO> userDAOMockedStatic = mockStatic(UserDAO.class)){
+            when(mockUserDao.getUserByEmail("test@gmail.com")).thenReturn(null);
+            userDAOMockedStatic.when(UserDAO::getInstance).thenReturn(mockUserDao);
+
+            assertThrows(ObjectNotFoundException.class,() ->{
+                SessionFacade.getInstance().login("test@gmail.com","wrongPassword");
+            });
+
+        }
+
+    }
+    @Test
+    public void login_userisBan_throwUserBannedException() {
+        try(MockedStatic<UserDAO> userDAOMockedStatic = mockStatic(UserDAO.class)){
+            User u = new User("test","test","test","RAaIRIIvNeeeEAvEANeIRNvvRNdvAiASIASeeNRieeiEAReSnnVeNRVSvIiERARVRNAIdieNIRIdeeeaAvAViReNSvvneevIevAVEAISeaIveeReAedARRvnndiidRAV","test","test","EIeeSVRARInSadadIVnAEARvENdReAdI",true);
+            u.setIsBanned(true);
+            when(mockUserDao.getUserByEmail("test@gmail.com")).thenReturn(u);
+            userDAOMockedStatic.when(UserDAO::getInstance).thenReturn(mockUserDao);
+
+            assertThrows(UserBannedException.class,() ->{
+                SessionFacade.getInstance().login("test@gmail.com","wrongPassword");
+            });
+
+
+        }
+
+    }
+
+    @Test
     public void login_goodPassword_returnUser() throws WrongPasswordException, ObjectNotFoundException, UserBannedException {
         try(MockedStatic<UserDAO> userDAOMockedStatic = mockStatic(UserDAO.class)){
             User u = new User("test","test","test","RAaIRIIvNeeeEAvEANeIRNvvRNdvAiASIASeeNRieeiEAReSnnVeNRVSvIiERARVRNAIdieNIRIdeeeaAvAViReNSvvneevIevAVEAISeaIveeReAedARRvnndiidRAV","test","test","EIeeSVRARInSadadIVnAEARvENdReAdI",true);
@@ -52,4 +82,5 @@ class SessionFacadeTest {
         }
 
     }
+
 }
